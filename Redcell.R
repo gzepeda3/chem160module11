d12df0fb15570df4bd30bf58e16d937e582f0e89
gzepeda3 <66925740+gzepeda3@users.noreply.github.com>
2020-10-22 21:36:23 -0700

    Add files via upload

---
+++ Redcell.R
@@ -0,0 +1,7 @@
+redcell<-read.table("redcell.txt", header=T)
+names(redcell)
+is.factor(redcell$ventilation)
+plot(folate~ventilation, data=redcell)
+model<-lm(folate~ventilation, data=redcell)
+summary(model)
+
